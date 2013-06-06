59c7461125ba6bde36e69af4ca23368b1a6daa92
Guillaume Turri <guillaume.turri@gmail.com>
2013-06-06 21:34:23 +0200

    Added test for column size

---
+++ _tests/src/nspages/printers/T_columnSize.java
@@ -0,0 +1,37 @@
+package nspages.printers;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import nspages.Helper;
+
+public class T_columnSize extends Helper {
+	@Test
+	public void longestColumnIsTheMostOnTheLeft(){
+		generatePage("ns1:start", "<nspages -exclude>");
+
+		assertEquals(2, columnSize(0));
+		assertEquals(1, columnSize(1));
+		assertEquals(1, columnSize(2));
+	}
+
+	@Test
+	public void smallestColumnIsTheMostOnTheRight(){
+		generatePage("ns1:start", "<nspages>");
+
+		assertEquals(2, columnSize(0));
+		assertEquals(2, columnSize(1));
+		assertEquals(1, columnSize(2));
+	}
+
+	private int columnSize(int idxCol){
+		WebElement column = getDriver().findElements(By.className("catpagecol")).get(idxCol);
+		List<WebElement> links = column.findElements(By.tagName("a"));
+		return links.size();
+	}
+}
