fca9c1d3352801e6130c88b6a9e962a4ef2bf550
Guillaume Turri <guillaume.turri@gmail.com>
2015-04-04 21:01:36 +0200

    Add sort By Date option
    Fix #31

---
+++ _tests/src/test/java/nspages/Test_sortDate.java
@@ -0,0 +1,35 @@
+package nspages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class Test_sortDate extends Helper {
+	@Test
+	public void withoutOption(){
+		generatePage("ns1:start", "<nspages -h1 -exclude>");
+
+		List<InternalLink> expectedLinks = new ArrayList<InternalLink>();
+		expectedLinks.add(new InternalLink("ns1:a", "a"));
+		expectedLinks.add(new InternalLink("ns1:b1", "b1"));
+		expectedLinks.add(new InternalLink("ns1:b2", "b2"));
+		expectedLinks.add(new InternalLink("ns1:c", "c"));
+
+		assertSameLinks(expectedLinks, getDriver());
+	}
+
+	@Test
+	public void withOption(){
+		generatePage("ns1:start", "<nspages -h1 -sortDate -exclude>");
+
+		List<InternalLink> expectedLinks = new ArrayList<InternalLink>();
+		expectedLinks.add(new InternalLink("ns1:a", "a"));
+		expectedLinks.add(new InternalLink("ns1:b2", "b2"));
+		expectedLinks.add(new InternalLink("ns1:c", "c"));
+		expectedLinks.add(new InternalLink("ns1:b1", "b1"));
+
+		assertSameLinks(expectedLinks, getDriver());
+	}
+
+}
