46cad9085e751dd1406a3389539f991fab5f2f27
Jan Müller <muller.jan@gmail.com>
2015-06-12 14:42:02 +0200

    Update syntax.php
    add options for hiding messages about empty subnamespace and pages

--- syntax.php
+++ syntax.php
@@ -56,6 +56,8 @@ class syntax_plugin_nspages extends DokuWiki_Syntax_Plugin {
         optionParser::checkOption($match, "pagesinns", $return['pagesinns'], true);
         optionParser::checkOption($match, "nat(ural)?Order", $return['natOrder'], true);
         optionParser::checkOption($match, "sort(By)?Date", $return['sortDate'], true);
+        optionParser::checkOption($match, "hidenopages", $return['hidenopages'], true);
+        optionParser::checkOption($match, "hidenosubns", $return['hidenosubns'], true);
         optionParser::checkRecurse($match, $return['maxDepth']);
         optionParser::checkNbColumns($match, $return['nbCol']);
         optionParser::checkTextPages($match, $return['textPages'], $this);
