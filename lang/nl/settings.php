0e113829f019ddb31b0126a1922165864c958284
hugo smet <hugo.smet@scarlet.be>
2015-08-02 00:20:33 +0200

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author hugo smet <hugo.smet@scarlet.be>
+ */
+$lang['cache']                 = 'uitschakelen tijdelijk geheugen (cache)';
