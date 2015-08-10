ca15574683425c5a02218ab054bd3b08d76ca777
Mark C. Prins <mprins@users.sf.net>
2015-08-10 13:20:38 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -4,10 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Mark Prins <mprins@users.sf.net>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
-$lang['doesntexist']           = 'deze naamruimte bestaat niet';
+$lang['doesntexist']           = 'deze naamruimte bestaat niet:';
 $lang['subcats']               = 'Subnaamruimte:';
 $lang['pagesinthiscat']        = 'Pagina\'s in deze naamruimte:';
 $lang['continued']             = 'verder';
--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -4,5 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author hugo smet <hugo.smet@scarlet.be>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 $lang['cache']                 = 'uitschakelen tijdelijk geheugen (cache)';
+$lang['toolbar_inserted_markup'] = 'Door de toolbarknop ingevoegde code';
