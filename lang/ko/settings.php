0a3b49db58eecbf03c502fe6566930099f55af20
Erial <erial2@gmail.com>
2015-12-15 03:15:38 +0100

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Erial <erial2@gmail.com>
+ */
+$lang['cache']                 = '캐시를 사용하지않습니다';
+$lang['toolbar_inserted_markup'] = '툴바에 기능 추가';
