ecfa86c3c726daca233a9b7ddb9773e7172535eb
Aleksandr Selivanov <alexgearbox@yandex.ru>
2017-11-03 16:20:06 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Alexey Markov <redrat@mail.ru>
  */
 $lang['encoding']              = 'utf-8';
--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['cache']                 = 'Отключить кэширование';
+$lang['toolbar_inserted_markup'] = 'Разметка для вставки через кнопку на панели инструментов страницы';
