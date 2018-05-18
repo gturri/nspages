8026c1c2170e7b8f7487dfe919814dfb7cdf5337
Sebastian Engel <mail@engel-s.de>
2018-05-18 13:10:12 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -2,14 +2,15 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Sebastian Engel <mail@engel-s.de>
  * @author Daniel Schranz <xla@gmx.at>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
-$lang['doesntexist']           = 'Dieser Namespace existiert nicht: ';
-$lang['subcats']               = 'Unterkategorien:';
-$lang['pagesinthiscat']        = 'Seiten in dieser Kategorie:';
+$lang['doesntexist']           = 'Dieser Namensraum existiert nicht: ';
+$lang['subcats']               = 'Unternamensraum:';
+$lang['pagesinthiscat']        = 'Seiten in diesem Namensraum:';
 $lang['continued']             = ' (Fortsetzung)';
-$lang['nopages']               = 'Keine Seiten in dieser Kategorie.';
-$lang['nosubns']               = 'Keine Unterkategorien.';
+$lang['nopages']               = 'Keine Seiten in diesem Namensraum.';
+$lang['nosubns']               = 'Keine Unternamensräume.';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,8 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Sebastian Engel <mail@engel-s.de>
  * @author Padhie <develop@padhie.de>
  */
 $lang['cache']                 = 'Cache deaktivieren';
+$lang['default_picture']       = 'Lege dein Standardbild fest';
 $lang['toolbar_inserted_markup'] = 'Toolbar eingefügtes markup';
