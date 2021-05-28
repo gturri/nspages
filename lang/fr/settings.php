22666491d5f9ffad7f6246dd0d9c24c1b1b979c5
Guillaume Turri <guillaume.turri@gmail.com>
2021-05-28 15:29:56 +0200

    Add FR translation

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,3 +4,4 @@ $lang['cache'] = 'Désactive le cache';
 $lang['default_picture'] = 'Image par défaut';
 $lang['toolbar_inserted_markup'] = 'Markup inséré par la barre d\'outils';
 $lang['custom_title_allow_list_metadata'] = 'Liste des métadata autorisées pour l\'option -customTitle';
+$lang['global_exclude'] = 'List d\'exclusion globale pour les pages et les catégories';
