b9abcb63a156f4a296cbe810ffc89277cfe52905
Vyacheslav Strenadko <bryanskmap@yandex.ru>
2022-11-23 17:35:30 +0100

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -3,9 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Vyacheslav Strenadko <bryanskmap@yandex.ru>
  * @author Konstantin Bobovskiy <kbobovskiy@yandex.ru>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['cache']                 = 'Отключить кэширование';
 $lang['default_picture']       = 'Установите ваше изображение по умолчанию';
 $lang['toolbar_inserted_markup'] = 'Разметка для вставки через кнопку на панели инструментов страницы';
+$lang['custom_title_allow_list_metadata'] = 'Метаданные списка разрешенных настраиваемых заголовков';
+$lang['global_exclude']        = 'Глобальный список исключений для страниц и пространств имен';
