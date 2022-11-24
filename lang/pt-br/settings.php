b23ceef2221daf559acf0d07f7b25e978780cf2a
nerun <nerunblogs@gmail.com>
2022-11-24 10:55:06 -0300

    Added brazilian portuguese (pt-br) translation.

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['cache'] = 'Desabilitar o cache';
+$lang['default_picture'] = 'Defina sua imagem padrão';
+$lang['toolbar_inserted_markup'] = 'Marcação inserida na barra de ferramentas';
+$lang['custom_title_allow_list_metadata'] = 'Lista de metadados permitidos para a opção -customTitle';
+$lang['global_exclude'] = 'Lista de exclusão global para páginas e domínios';
