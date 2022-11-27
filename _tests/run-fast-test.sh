2631615d70e9be5f7efe9a0e6f9640b93211d6fb
Guillaume Turri <guillaume.turri@gmail.com>
2022-11-27 20:49:27 +0100

    Fix test run on Ubuntu 2022-04

--- _tests/run-fast-test.sh
+++ _tests/run-fast-test.sh
@@ -5,5 +5,11 @@
 ./internal/dl_dw.sh
 sudo ./internal/installTestEnvironment.sh
 
+# Fix for the issue described on https://github.com/mozilla/geckodriver/issues/2010
+# (needed to run on Ubuntu 2022-04 since Firefox is installed from Snap)
+export TMPDIR="$HOME/temp-firefox-profile-for-nspages-tests"
+echo "Using temporary dir $TMPDIR for firefox profile (you can delete it after the tests)"
+mkdir -p $TMPDIR
+
 # "grep -v" to discard some useless and noisy log
 mvn test 2>&1 | grep -v "console.warn: LoginRecipes:"
