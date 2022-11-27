ef6f4e086a4caa48d1c6e1285c3815e82f71627b
Guillaume Turri <guillaume.turri@gmail.com>
2022-11-27 22:19:26 +0100

    Fix test

--- _tests/src/test/java/nspages/Test_customTitle.java
+++ _tests/src/test/java/nspages/Test_customTitle.java
@@ -8,6 +8,14 @@ import java.util.List;
 public class Test_customTitle extends Helper {
     @Test
     public void withSimpleOption() {
+        // Start with a page with just the title to ensure the metadata is set, otherwise we'll have a PHP warning because we try to access a metadata
+        // which has not been set yet.
+        // (it's perhaps a workaround against the more general issue of tackling missing metadata. Perhaps having a fallback to something else could
+        // make more sense. But OTHO it's not trivial to find out a sensible metadata in the general case, and it's not even sure that's what users would
+        // want: perhaps we could assume that it's the responsibility of users who want to use a given metadata to ensure it's set)
+        generatePage("customtitle:start", "======Some Title======");
+
+        // now we can start running the test
         generatePage("customtitle:start", "======Some Title======\n\n"
         + "<nspages -customTitle=\"Custom title for page {title}\" >");
 
