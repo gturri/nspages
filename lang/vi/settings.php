5be23a9fec478541a849e9fea3045ee94448c79d
Minh <phandinhminh@protonmail.ch>
2023-01-24 15:35:14 +0100

    translation update

---
+++ lang/vi/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['cache']                 = 'Tắt bộ đệm cache';
+$lang['default_picture']       = 'Đặt hình ảnh mặc định của bạn';
+$lang['toolbar_inserted_markup'] = 'Đánh dấu đã thêm thanh công cụ ';
+$lang['custom_title_allow_list_metadata'] = 'Tiêu đề tùy chỉnh cho phép danh sách siêu dữ liệu metadata';
+$lang['global_exclude']        = 'Danh sách loại trừ toàn cầu cho các trang và không gian tên namespace';
