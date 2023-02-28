df1386cdad1c8610560fb514e81de1c24312deb4
Gerrit <klapinklapin@gmail.com>
2023-02-28 21:30:17 +0100

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -3,10 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author Gerrit <klapinklapin@gmail.com>
  * @author hugo smet <hugo.smet@scarlet.be>
  * @author Mark C. Prins <mprins@users.sf.net>
  */
 $lang['cache']                 = 'uitschakelen tijdelijk geheugen (cache)';
 $lang['default_picture']       = 'Stel je standaardafbeelding in';
 $lang['toolbar_inserted_markup'] = 'Door de toolbarknop ingevoegde code';
+$lang['custom_title_allow_list_metadata'] = 'Aangepaste titel mag metadata laten zien';
+$lang['global_exclude']        = 'Globale uitsluiting van pagina\'s en naamruimtes';
