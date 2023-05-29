8fdcd80627e6bd738c0bf3c847feb7a456d2f209
Daniel Dias Rodrigues <danieldiasr@gmail.com>
2023-05-29 20:30:38 +0200

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Brazilian portuguese language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Daniel Dias Rodrigues <danieldiasr@gmail.com>
+ *
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
  */
-
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
 $lang['doesntexist']           = 'este domínio não existe: ';
@@ -14,3 +15,4 @@ $lang['pagesinthiscat']        = 'Páginas neste domínio:';
 $lang['continued']             = ' cont.';
 $lang['nopages']               = 'Nenhuma página neste domínio.';
 $lang['nosubns']               = 'Nenhum subdomínio.';
+$lang['sidebarOrNs']           = 'Com a opção -sidebar, você não pode especificar um domínio. Encontrado:';
