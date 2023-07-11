00d6aff7d43111ed5292158d793f600e2e9894c0
Sevynidd <sevynidd02@protonmail.com>
2023-07-11 13:20:26 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,10 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Sevynidd <sevynidd02@protonmail.com>
  * @author Sebastian Engel <mail@engel-s.de>
  * @author Padhie <develop@padhie.de>
  */
 $lang['cache']                 = 'Cache deaktivieren';
 $lang['default_picture']       = 'Lege dein Standardbild fest';
 $lang['toolbar_inserted_markup'] = 'Toolbar eingefügtes markup';
-$lang['global_exclude'] = 'Globale Ausschließungsliste für Seiten und Namensräume';
+$lang['custom_title_allow_list_metadata'] = 'Benutzerdefinierter Titel ermöglicht Metadatenliste';
+$lang['global_exclude']        = 'Globale Ausschließungsliste für Seiten und Namensräume';
