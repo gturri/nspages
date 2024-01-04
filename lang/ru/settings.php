aecb58f0aef1c98490f3dc0df07bcf52faecc009
Aleksandr Selivanov <alexgearbox@yandex.ru>
2024-01-04 23:25:42 +0100

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -3,12 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  * @author Vyacheslav Strenadko <bryanskmap@yandex.ru>
  * @author Konstantin Bobovskiy <kbobovskiy@yandex.ru>
- * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['cache']                 = 'Отключить кэширование';
-$lang['default_picture']       = 'Установите ваше изображение по умолчанию';
+$lang['default_picture']       = 'Укажите своё изображение по умолчанию';
 $lang['toolbar_inserted_markup'] = 'Разметка для вставки через кнопку на панели инструментов страницы';
-$lang['custom_title_allow_list_metadata'] = 'Метаданные списка разрешенных настраиваемых заголовков';
-$lang['global_exclude']        = 'Глобальный список исключений для страниц и пространств имен';
+$lang['custom_title_allow_list_metadata'] = 'Метаданные списка разрешённых настраиваемых заголовков';
+$lang['global_exclude']        = 'Глобальный список исключений для страниц и пространств имён';
